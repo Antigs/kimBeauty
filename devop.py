b49ae75250ac410a48736d1e59c5d31b19183b39
Kimbeauty1 <ukpongdeborah44@gmail.com>
2024-08-26 17:16:36 +0100

    Add files via upload

---
+++ devop.py
@@ -0,0 +1,12 @@
+#dictionary
+student={'name': ['sam', 'joy', 'gold']}
+print(student.get('name')[1][2])
+print(student.get('name')[1:3])
+
+
+
+# dictionary
+student={'class':('pri 1','pri 2','pri 3')}
+print(student.get('class')[0][4])
+print(student.get('class')[1])
+print(student.get('class')[1][3])
