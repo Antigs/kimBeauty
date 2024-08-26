e09e2bb3123c95d0f262a4b968bdac742c0e0622
Kimbeauty1 <ukpongdeborah44@gmail.com>
2024-08-26 17:15:22 +0100

    Newfile

---
+++ newfile.py
@@ -0,0 +1,9 @@
+#create a simple python program consisting of datatype,data structure,funtions
+
+def school(students, class_name):
+       students=input('enter your students name\n')
+       class_name=input('enter your class_name\n')
+       
+       print(students, class_name)
+       
+school=(any)
